7cce9b8b709af80fa24c0035beebe6c3f2d51460
Jonas Galvez <jonasgalvez@gmail.com>
2019-06-25 21:40:20 -0300

    add blog-only example (2)

---
+++ examples/blog-only/blog/older.md
@@ -0,0 +1,7 @@
+Jun 20, 2019
+
+-
+
+# A sample old entry
+
+Here's a backdated entry just to fill the archive.
--- src/blueprints/common/pages/source.vue
+++ src/blueprints/common/pages/source.vue
@@ -8,17 +8,14 @@
 <script>
 const components = {}
 
-// The component key differs from the actual component name
-// because the key is also used to determine the layout
-
 <% if (options.$docs) { %> 
 import PressTopic from '../../docs/pages/topic'
-components['press-docs'] = PressTopic
+components['press-topic'] = PressTopic
 <% } %>
 
 <% if (options.$blog) { %> 
 import PressEntry from '../../blog/pages/entry'
-components['press-blog'] = PressEntry
+components['press-entry'] = PressEntry
 <% } %>
 
 <% if (options.$slides) { %> 
@@ -26,12 +23,18 @@ import PressSlides from '../../slides/pages/slides'
 components['press-slides'] = PressSlides
 <% } %>
 
+const layoutTypeMap = {
+  'press-topic': 'docs',
+  'press-entry': 'blog',
+  'press-slides': 'slides'
+}
+
 export default {
   components,
   layout({ params }) {
     const cKeys = Object.keys(components)
     if (cKeys.length === 1) { // single-mode
-      return cKeys[0].slice(cKeys[0].indexOf('-') + 1)
+      return layoutTypeMap[cKeys[0]]
     } else {
       return params.source.slice(0, params.source.indexOf('/'))
     }
