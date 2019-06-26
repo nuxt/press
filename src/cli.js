a30994c6eb43c6729430eef0b0c591c47b3deb32
Jonas Galvez <jonasgalvez@gmail.com>
2019-06-26 01:58:29 -0300

    ongoing work on eject (2)

--- src/cli.js
+++ src/cli.js
@@ -1,21 +1,47 @@
 import consola from 'consola'
 
+import {
+  ensureDir,
+  join,
+  dirname,
+  writeFile,
+  readFile
+} from './utils'
+
 import docs from './blueprints/docs'
 import blog from './blueprints/blog'
 import slides from './blueprints/slides'
 import common from './blueprints/common'
 
+const cwd = process.cwd()
 const blueprints = { docs, blog, slides, common }
 
+async function ejectTemplate(path) {
+  const blueprintsPath = join(dirname(require.resolve(`@nuxt/press`)), 'blueprints')
+  await ensureDir(join(cwd, 'press', dirname(path)))
+  await writeFile(
+    join(cwd, 'press', path),
+    await readFile(join(blueprintsPath, path))
+  )
+  consola.info(`Ejected press/${path}`)
+}
+
 class commands {
-  static eject(...args) {
+  static async eject(args) {
     const [ blueprint, template ] = args[0].split('/')
     if (blueprint in blueprints) {
       if (template) {
-        const templatePath = join(blueprint, blueprints[blueprint].templates[template])
+        if (!blueprints[blueprint].templates[template]) {
+          consola.fatal('Unrecognized template -- please see docs at https://nuxt.press/')
+          process.exit()
+        }
+        await ejectTemplate(join(blueprint, blueprints[blueprint].templates[template]))
       } else {
-        for (let bTemplate of Object.values(blueprints[blueprint].templates)) {
-
+        for (const bTemplate of Object.values(blueprints[blueprint].templates)) {
+          if (typeof bTemplate !== 'string') {
+            continue
+          }
+          await ejectTemplate(join(blueprint, bTemplate))
         }
       }
     }
@@ -24,13 +50,13 @@ class commands {
 
 export default {
   name: 'press',
-  description: 'CLI for @nuxt/press',
+  description: 'CLI for NuxtPress',
   usage: 'press <cmd>',
-  run(cmd) {
+  async run(cmd) {
     if (cmd.argv._.length && cmd.argv._[0] in commands) {
-      commands[cmd.argv._[0]](cmd.argv._.slice(1))
+      await commands[cmd.argv._[0]](cmd.argv._.slice(1))
     } else {
-      console.log('Unrecognized command -- please see docs at https://nuxt.press/')
+      consola.fatal('Unrecognized command -- please see docs at https://nuxt.press/')
     }
   }
 }
