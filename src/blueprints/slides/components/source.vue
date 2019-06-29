4a37474b20e1cc8f547d0adf5e11573542c1b89f
Jonas Galvez <jonasgalvez@gmail.com>
2019-06-28 22:27:40 -0300

    restructuring templates/configs (3)

---
+++ src/blueprints/slides/components/source.vue
@@ -0,0 +1,71 @@
+<template>
+  <no-ssr>
+    <swiper
+      ref="slides"
+      :options="swiperOptions"
+      :class="`slides-${path.split('/')[1]}`"
+      @slideChange="setCurrentSlide"
+    >
+      <swiper-slide
+        v-for="(item, slideIndex) in data.slides"
+        :key="`slide-${slideIndex}`"
+        class="slide"
+        :class="`slide-${slideIndex+1}`"
+      >
+        <div v-html="item" />
+      </swiper-slide>
+
+      <div class="swiper-pagination" slot="pagination"></div>
+      <div class="swiper-button-prev" slot="button-prev"></div>
+      <div class="swiper-button-next" slot="button-next"></div>
+    </swiper>
+  </no-ssr>
+</template>
+
+<script>
+import { startObserver } from '../../common/components/observer'
+
+export default {
+  props: ['data', 'path'],
+  data: () => ({
+    currentSlide: -1,
+    // http://idangero.us/swiper/api/#initialize
+    swiperOptions: {
+      mousewheel: true,
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'progressbar'
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      }
+    }
+  }),
+  watch: {
+    currentSlide(index) {
+      const currentRoute = `${this.$route.path}#${index + 1}`
+
+      this.$press.disableScrollBehavior = true
+      this.$router.replace(currentRoute, () => {
+        this.$nextTick(() => {
+          this.$press.disableScrollBehavior = false
+        })
+      })
+    }
+  },
+  beforeMount() {
+    this.swiperOptions.initialSlide = Math.max(parseInt(this.$route.hash.substr(1)) - 1 || 0, 0)
+
+    import('keymaster').then(({ default: keymaster }) => {
+      keymaster('right', () => this.$refs.slides.swiper.slideNext())
+      keymaster('left', () => this.$refs.slides.swiper.slidePrev())
+    })
+  },
+  methods: {
+    setCurrentSlide() {
+      this.currentSlide = this.$refs.slides && this.$refs.slides.swiper.activeIndex
+    }
+  }
+}
+</script>
