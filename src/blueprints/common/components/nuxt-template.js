033aec61832287cbcb37c954fff6ec7178dd65e0
pimlie <pimlie@hotmail.com>
2019-07-11 09:25:17 +0200

    fix(common): only support v-model for nuxt-template

--- src/blueprints/common/components/nuxt-template.js
+++ src/blueprints/common/components/nuxt-template.js
@@ -10,29 +10,9 @@ export default {
       default: 'div'
     }
   },
-  render(h, { props, children }) {
-    let html
-    if (children && children.length) {
-      html = children.reduce((acc, c) => (acc += c.text), '')
-    } else {
-      html = props.value
-    }
-
+  render(h, { props }) {
     return h({
-      template: `<${props.tag}>${html}</${props.tag}>`
+      template: `<${props.tag}>${props.value}</${props.tag}>`
     })
   }
-  /* created() {
-    if (this.$slots.default) {
-      this.html = ''
-      for (const slot of this.$slots.default) {
-        this.html += slot.text
-      }
-    } else {
-      this.html = this.value
-    }
-    this.$watch('value', (html) => {
-      this.html = html
-    })
-  } */
 }
