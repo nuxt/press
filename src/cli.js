47637fdf22cbf9d6860b838ba7751975a63ed261
Jonas Galvez <jonasgalvez@gmail.com>
2019-07-13 00:40:35 -0300

    fix(cli): update eject template path (2)

--- src/cli.js
+++ src/cli.js
@@ -26,14 +26,14 @@ async function ejectTheme(path) {
   consola.info(`Ejected to ./nuxt.press.css`)
 }
 
-async function ejectTemplate(blueprintPath, templatePath) {
+async function ejectTemplate(blueprint, templatePath) {
   const blueprintsPath = join(dirname(require.resolve(`@nuxt/press`)), 'blueprints')
-  await ensureDir(join(cwd, 'press', dirname(templatePath)))
+  await ensureDir(join(cwd, 'press', blueprint, dirname(templatePath)))
   await writeFile(
-    join(cwd, 'press', templatePath),
-    await readFile(join(blueprintsPath, blueprintPath, 'templates', templatePath))
+    join(cwd, 'press', blueprint, templatePath),
+    await readFile(join(blueprintsPath, blueprint, 'templates', templatePath))
   )
-  consola.info(`Ejected press/${templatePath}`)
+  consola.info(`Ejected ${join('press', blueprint, templatePath)}`)
 }
 
 class commands {
