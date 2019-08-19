cae868ef6b8adb443f5393179259dc3a19e9ab5d
pimlie <pimlie@hotmail.com>
2019-08-19 11:28:19 +0200

    test: improve coverage config

--- jest.config.js
+++ jest.config.js
@@ -12,23 +12,15 @@ module.exports = {
   coverageDirectory: './coverage',
 
   collectCoverageFrom: [
-    'src/**/*.js'
-  ],
-
-  coveragePathIgnorePatterns: [
-    'node_modules',
-    '/templates/',
-    '/test/',
+    'src/**/*.js',
+    '!**/templates/**',
+    '!**/test/**'
   ],
 
   moduleNameMapper: {
     "@/(.*)$": "<rootDir>/test/$1",
     "~/(.*)$": "<rootDir>/src/$1",
-    "^pressModule$": stdEnv.ci ? "<rootDir>/" : "<rootDir>/src/",
-    "^press/blog/(.*)$": "<rootDir>/src/blueprints/blog/templates/$1",
-    "^press/docs/(.*)$": "<rootDir>/src/blueprints/docs/templates/$1",
-    "^press/common/(.*)$": "<rootDir>/src/blueprints/common/templates/$1",
-    "^press/slides/(.*)$": "<rootDir>/src/blueprints/slides/templates/$1"
+    "^pressModule$": stdEnv.ci ? "<rootDir>/" : "<rootDir>/src/"
   },
 
   transform: {
